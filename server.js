3b849bcf02999790c5c04d5baed70d30f44de528
Mahela <lenken125@gmail.com>
2024-09-22 11:45:55 +1000

    seventh commit

--- server.js
+++ server.js
@@ -10,32 +10,37 @@ app.use("/api", apicontroller);
 //to serve static files
 app.use(express.static(__dirname));
 
-//rote to server index.html
+//route to server index.html
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/html/index.html'))
 });
 
-//rote to server search.html
+//route to server search.html
 app.get('/search',(req,res)=>{
     res.sendFile(path.join(__dirname,'/html/search.html'))
 });
 
+//route to server search.js
 app.get('/search.js',(req,res)=>{
     res.sendFile(path.join(__dirname,'/html/search.js'))
 });
 
+//route to server styles.css
 app.get('/styles.css',(req,res)=>{
     res.sendFile(path.join(__dirname,'/html/styles.css'))
 });
 
+//route to server home.js
 app.get('/home.js',(req,res)=>{
     res.sendFile(path.join(__dirname,'/html/home.js'))
 });
 
+//route to server fundraiser.html
 app.get('/fundraiser',(req,res)=>{
     res.sendFile(path.join(__dirname,'/html/fundraiser.html'))
 });
 
+//route to server fundraiser.js
 app.get('/fundraiser.js',(req,res)=>{
     res.sendFile(path.join(__dirname,'/html/fundraiser.js'))
 });
